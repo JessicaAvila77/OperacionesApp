7e3a238fd8d8481351386587da78ab135093bcab
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-09-03 16:17:47 -0600

    trabajando en backend y frontend

--- OperacionesApp/MainPage.xaml.cs
+++ OperacionesApp/MainPage.xaml.cs
@@ -9,7 +9,51 @@
             InitializeComponent();
         }
 
-        
+        private void txtCalcularCuadrado_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                double lado, area;                
+
+                if (ladoCuadrado.Text is null || ladoCuadrado.Text == "")
+                {
+                    DisplayAlert("ADVERTENCIA", "Ingrese el valor del lado", "Aceptar");
+                }
+                else
+                {
+                    lado = double.Parse(ladoCuadrado.Text);
+
+                    if (lado <= 0)
+                    {
+                        DisplayAlert("ADVERTENCIA", "El valor del lado no puede ser negativo o igual a cero", "Aceptar");
+                    }
+                    else
+                    {
+                        area = lado * lado;
+
+                        respCuadrado.Text = area.ToString();
+                    }
+
+                } 
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ERROR", ex.Message, "Aceptar");
+            }
+        }
+
+
+
+        private void txtLimpiarCuadrado_Clicked(object sender, EventArgs e)
+        {
+
+            ladoCuadrado.Text = "";
+            respCuadrado.Text = "";
+
+        }
+
+
     }
 
 }
